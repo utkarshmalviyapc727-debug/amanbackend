31a4077dfb98b13b8006956140539b4332ed5404
utkarshmalviyapc727-debug <utkarshmalviyapc727@gmail.com>
2026-03-18 00:55:45 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    firebaseUid: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('User', userSchema);
