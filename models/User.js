01541affd9e7274fbedf971d61fb88a987b20e47
utkarshmalviyapc727-debug <utkarshmalviyapc727@gmail.com>
2026-03-18 02:02:58 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+
 const userSchema = new mongoose.Schema({
     firebaseUid: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    name: { type: String, default: "" },
+    dob: { type: String, default: "" },
+    studentUid: { type: String, default: "" },
+    contact: { type: String, default: "" },
     createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('User', userSchema);
